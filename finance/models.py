8af890134685e601ed4c9c1a6645845f23dbe17f
jtthiago <jtthiago42@outlook.com>
2025-01-25 05:21:58 -0300

    Criando o Modelo Inicial para Transações

--- finance/models.py
+++ finance/models.py
@@ -3,12 +3,13 @@ from django.db import models
 # Create your models here.
 
 class Transaction(models.Model):
+    # Define os tipos de transação possíveis (receita ou despesa)
     TRANSACTION_TYPES = (
         ('income', 'Receita'),
         ('expense', 'Despesa'),
 
     )
-
+    
     title = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     transaction_type = models.CharField(max_length=7, choices=TRANSACTION_TYPES)
@@ -17,4 +18,10 @@ class Transaction(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    # Retorna uma string representando o título e o tipo da transação
+    def __str__(self):
+        return f"{self.title} - {self.transaction_type.capitalize()}"
     
+
+
+
