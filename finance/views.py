c1d2c6e5aeae6ed2d8004d6502676ed368ba2cc6
jtthiago <jtthiago42@outlook.com>
2025-01-29 10:26:45 -0300

    Criei as Views no App finance No arquivo views.py do app finance, criei as views baseadas em classe para gerenciar transações

--- finance/views.py
+++ finance/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .models import Transaction
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class TransactionList(ListView):
+    model = Transaction
+    template_name = 'finance/transaction_list.html'
+    context_object_name = 'transactions'
+
+
+class TransactionCreate(CreateView):
+    model = Transaction
+    fields = ['description', 'amount', 'category', 'date']
+    template_name = 'finance/transaction_form.html'
+    success_url = reverse_lazy('finance:transaction_list')
+
+class TransactionUpdate(UpdateView):
+    model = Transaction
+    fields = ['description', 'amount', 'category', 'date']
+    template_name = 'finance/transaction_form.html'
+    success_url = reverse_lazy('finance:transaction_list')
+
+class TransactionDelete(DeleteView):
+    model = Transaction
+    template_name = 'finance/transaction_confirm_delete.html'
+    success_url = reverse_lazy('finance:transaction_list')
