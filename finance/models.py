89fd51ae3085f8bdde2f2e8a2ee635a03e1206b0
jtthiago <jtthiago42@outlook.com>
2025-01-31 16:10:20 -0300

    ajustando erro nos links e configurando o front end com bootstrap

--- finance/models.py
+++ finance/models.py
@@ -11,7 +11,7 @@ class Transaction(models.Model):
     )
     
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, default="Sem descrição")
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     transaction_type = models.CharField(max_length=7, choices=TRANSACTION_TYPES)
     category = models.CharField(max_length=50)
--- project/urls.py
+++ project/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('finance/', include('finance.urls')),
+
+    # Redireciona '/' para '/finance/'
+    path('', lambda request: redirect('finance:transaction_list')),
 ]
