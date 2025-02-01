177c8cf9252cca3dd2f007f41ec2662b985bffce
jtthiago <jtthiago42@outlook.com>
2025-02-01 16:40:14 -0300

    Foi atualizando a pagina principal, incluindo novos campos de cadastro. Foi retirado a pagina delete e incluido uma mensagem quando o usuario for deletar a transação

--- finance/urls.py
+++ finance/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 from . import views
 
-
 app_name = 'finance'
 
 urlpatterns = [
     path('', views.TransactionList.as_view(), name='transaction_list'),
     path('create/', views.TransactionCreate.as_view(), name='transaction_create'),
     path('<int:pk>/update/', views.TransactionUpdate.as_view(), name='transaction_update'),
-    path('<int:pk>/delete/', views.TransactionDelete.as_view(), name='transaction_delete'),
+    path('<int:pk>/delete/', views.transaction_delete, name='transaction_delete'),  # Removidos os parênteses
 ]
-
--- finance/views.py
+++ finance/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
+from django.views.generic import ListView, CreateView, UpdateView
 from .models import Transaction
 from django.urls import reverse_lazy
 
@@ -11,17 +12,18 @@ class TransactionList(ListView):
 
 class TransactionCreate(CreateView):
     model = Transaction
-    fields = ['description', 'amount', 'category', 'date']
+    fields = ['title','description', 'amount', 'transaction_type', 'category', 'date']
     template_name = 'finance/transaction_form.html'
     success_url = '/finance/'
 
 class TransactionUpdate(UpdateView):
     model = Transaction
-    fields = ['description', 'amount', 'category', 'date']
+    fields = ['title','description', 'amount', 'transaction types', 'category', 'date']
     template_name = 'finance/transaction_form.html'
     success_url = reverse_lazy('finance:transaction_list')
 
-class TransactionDelete(DeleteView):
-    model = Transaction
-    template_name = 'finance/transaction_confirm_delete.html'
-    success_url = reverse_lazy('finance:transaction_list')
+def transaction_delete(request, pk):
+    transaction = get_object_or_404(Transaction, pk=pk)
+    transaction.delete()
+    messages.success(request, "Transação excluída com sucesso!")
+    return redirect('finance:transaction_list')
