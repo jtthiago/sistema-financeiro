9c1ddbc77438f06bb4e3f8eb6a6908aefcdb23d1
jtthiago <jtthiago42@outlook.com>
2025-02-08 15:21:15 -0300

    transferindo o css para uma pasta separada do html

--- project/settings.py
+++ project/settings.py
@@ -125,6 +125,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # Agora os arquivos estáticos ficam diretamente em "static/"
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
